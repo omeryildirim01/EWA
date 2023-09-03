c730db7cafa411c44be89ebfb864f3b2258f84b3
Omer Yildirim <omer.yildirim@commencis.com>
2023-09-03 08:58:21 +0300

    hilt dependency added

---
+++ app/src/main/java/com/yildirimomer01/ewa/core/Constants.kt
@@ -0,0 +1,4 @@
+package com.yildirimomer01.ewa.core
+
+object Constants {
+}
---
+++ app/src/main/java/com/yildirimomer01/ewa/core/EWApp.kt
@@ -0,0 +1,7 @@
+package com.yildirimomer01.ewa.core
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class EWApp : Application()
