4265dd2f99ae435d2065b5fe783c58016eb7853c
Omer Yildirim <omer.yildirim@commencis.com>
2023-09-05 23:08:43 +0300

    removed unnecessary components

--- app/src/main/java/com/yildirimomer01/ewa/util/Extensions.kt
+++ app/src/main/java/com/yildirimomer01/ewa/util/Extensions.kt
@@ -2,16 +2,6 @@ package com.yildirimomer01.ewa.util
 
 import retrofit2.Response
 
-fun <T> Result<T>.asResource(): Resource<T> {
-    return if (this.isSuccess) {
-        Resource.Success<T>(data = this.getOrNull())
-    } else {
-        Resource.Failure<T>(
-            message = this.exceptionOrNull()?.localizedMessage ?: "Runtime Exception"
-        )
-    }
-}
-
 fun <T> Response<T>.asResource(): Resource<T> {
     return if (isSuccessful) {
         body()?.let {
