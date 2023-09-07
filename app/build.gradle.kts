5c77d403999703d84b05ea223fad9aadbc40eedb
Omer Yildirim <omer.yildirim@commencis.com>
2023-09-07 10:22:29 +0300

    fixed CI build issues, updated some dependency versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,10 +99,14 @@ dependencies {
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
-    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.10.0"))
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    // define a BOM and its version
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.11.0"))
+
+    // define any required OkHttp artifacts without version
     implementation("com.squareup.okhttp3:okhttp")
     implementation("com.squareup.okhttp3:logging-interceptor")
-    testImplementation("com.squareup.okhttp3:mockwebserver:4.10.0")
+    testImplementation("com.squareup.okhttp3:mockwebserver:4.11.0")
 
     val hiltVersion = "2.47"
     // Hilt
